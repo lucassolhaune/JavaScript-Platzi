acdf2a4d448bae3382814cbd03f1f16660c3ca07
Lucas Solhaune <lu.solhaune@gmail.com>
2024-04-16 11:32:04 -0300

    Ejercicio utilizando las clases null-undefined-symbol y bigint

---
+++ ej10/clase-null-undefined-symbol-bigint.js
@@ -0,0 +1,30 @@
+//null
+const snoopy = null
+console.log(snoopy)
+console.log(typeof snoopy)
+
+//undefined
+var name
+console.log(name)
+
+//symbol
+const undqueId = Symbol('id')
+const symbol1 = Symbol(1)
+const symbol2 = Symbol(2)
+console.log(symbol1 === symbol2)
+
+const ID = Symbol ('id')
+var user = {}
+user[ID] = '1234' //A user le asignamos una key que es el valor "1234"
+console.log(user)
+
+//Bigint Numeros grandes
+const bigNumber = 20394239407284n
+console.log(bigNumber)
+
+const numberOfParticlesInTheUniverse = 10000000000000000000000000n
+console.log(numberOfParticles)
+
+
+
+
