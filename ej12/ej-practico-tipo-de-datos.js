75372963a154d91ee4bdcf3d3b8e07c241ec8cf1
Lucas Solhaune <lu.solhaune@gmail.com>
2024-04-17 14:51:54 -0300

    ejercicio de tipos de datos

---
+++ ej12/ej-practico-tipo-de-datos.js
@@ -0,0 +1,21 @@
+//Social media Profile
+
+//1- User information
+const username = "Solhaune"
+const fullName = "Lucas Solhaune"
+const age = 23
+const isStudent = true
+
+//2- Address -> OBJETO
+const address = {
+    street : "Junin 270",
+    city: "Rosario",
+    state: "Codingland",
+    zipCode: 2000,
+
+}
+//3- hobbies
+const hobbies = ["coding", "reading", "gaming"]
+
+//4- generating personalized bio
+
